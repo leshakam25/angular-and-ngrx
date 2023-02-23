03f03384578d6e0f8755cef857bbad52bba30651
leshakam25 <leshakam25@gmail.com>
2023-02-23 22:10:20 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import {EffectsModule} from '@ngrx/effects'
 import {TopBarModule} from 'src/app/shared/modules/topBar/topBar.module'
 import {PersistanceService} from './shared/services/persistance.service'
 import {AuthInterceptor} from './shared/services/authinterceptor.service'
-import {GlobalFeedModule} from './globalFeed/globalFeed.module';
+import {GlobalFeedModule} from './globalFeed/globalFeed.module'
 
 @NgModule({
   declarations: [AppComponent],
